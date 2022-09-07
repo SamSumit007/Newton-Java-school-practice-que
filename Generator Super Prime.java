8f6a5442f00571ed46d80cfe915d686761a26382
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-09-07 16:15:37 +0530

    Create Generator Super Prime.java

---
+++ Generator Super Prime.java
@@ -0,0 +1,19 @@
+Sample Input:-
+5
+
+Sample Output:-
+3 4 5
+
+Sample Input:-
+4
+
+Sample Output:-
+3 4
+
+
+
+
+public static void SuperPrimes(int n){
+for (int i = n/2+1; i<=n; i++)
+    System.out.print(i+" ");
+    }
